e03f31d0947f410dd74a61a89de3b0562db35f80
dpetrov835 <164385826+dpetrov835@users.noreply.github.com>
2026-04-24 21:15:28 +0300

    Update bc_hcubewise.py

--- bc_hcubewise.py
+++ bc_hcubewise.py
@@ -1,5 +1,8 @@
 import argparse
-import sys
+import numpy as np
+import scipy as sp
+from sklearn.preprocessing import PowerTransformer
+from collections import defaultdict
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--hypercubes')
@@ -7,31 +10,61 @@ parser.add_argument('-l', '--landscape')
 parser.add_argument('-o', '--output')
 args = parser.parse_args()
 
-# this is the box-cox linearization script for hypercube-wise computation
+# This is the Box-Cox linearization script for hypercube-wise computation
 
 land = dict()
 
-with open(args.landscape) as f:  # parsing the landscape file (hash table format)
+with open(args.landscape) as f:  # Parse the landscape file (genotypes in sequence format)
     for line in f:
         spl = line.split()
         land[spl[0]] = spl[1]
 
-import numpy as np
-import scipy as sp
-import math
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import PowerTransformer
-import pwlf
+def model(Padd: np.array[float], lmbda: float, A: float, B: float):  # Define the Box-Cox based transform
+    """
+    
 
-def model(Padd, lmbda, A, B):   # defining the box-cox based transform
-    return ((Padd + A) ** lmbda - 1) / (lmbda * sp.stats.gmean(Padd + A) ** (lmbda - 1)) + B if lmbda !=0 else sp.stats.gmean(Padd + A)*np.log(Padd + A) + B
+    Parameters
+    ----------
+    Padd : np.array[float]
+        ADDITIVE PHENOTYPES.
+    lmbda : float
+        LMBDA PARAMETER.
+    A : float
+        A PARAMETER.
+    B : float
+        B PARAMETER.
+
+    Returns
+    -------
+    np.array[float]
+        OBSERVED PHENOTYPES.
+
+    """
+    if lmbda !=0:
+        return ((Padd + A) ** lmbda - 1) / (lmbda * sp.stats.gmean(Padd + A) ** (lmbda - 1)) + B
+    else:
+        return sp.stats.gmean(Padd + A) * np.log(Padd + A) + B
 
 genotypes = list()
 Pobs = list()
 
-from collections import defaultdict
+def calculate_additive_phenotypes(genotypes: list[str], phenotypes: list[float]):    # Calculate additive phenotypes
+    """
+    
 
-def calculate_additive_phenotypes(genotypes, phenotypes):    # calculating additive phenotypes
+    Parameters
+    ----------
+    genotypes : list[str]
+        GENOTYPES.
+    phenotypes : list[float]
+        OBSERVED PHENOTYPES.
+
+    Returns
+    -------
+    list[float]
+        ADDITIVE PHENOTYPES.
+
+    """
     n = len(genotypes)
     if n == 0:
         return []
@@ -60,6 +93,7 @@ def calculate_additive_phenotypes(genotypes, phenotypes):    # calculating addit
     for key, lst in effects_lists.items():
         if lst:
             effects[key] = sum(lst) / len(lst)
+    print(effects)
     additive_phenotypes = []
     for g in genotypes:
         add = wt_phen
@@ -83,31 +117,36 @@ with open(args.hypercubes) as fin, open(args.output, 'w') as fout:
             temp = list()
             for seq in genotypes:
                 temp.append(seq[:int(mut[1:-1])]+mut[0]+seq[int(mut[1:-1])+1:])
-            genotypes.extend(temp) # defining each hypercube in situ
+            genotypes.extend(temp) # Define each hypercube in situ
 
-        Pobs = list(map(float, [land[genotype] for genotype in genotypes])) # finding phenotypes for needed genotypes
+        Pobs = list(map(float, [land[genotype] for genotype in genotypes])) # Find phenotypes for needed genotypes
         Padd = np.array(calculate_additive_phenotypes(genotypes, Pobs))
 
         pt = PowerTransformer()
         pt.fit(Padd.reshape(-1, 1))
-        lambdas = pt.lambdas_ # initial guess for lambda parameter
+        lambdas = pt.lambdas_ # Initial guess for lambda parameter
         print(lambdas)
         try:
             popt, pcov = sp.optimize.curve_fit(f=model, xdata=Padd, ydata=Pobs, sigma=0.01, p0=[lambdas[0],0,0],   
-                bounds=([0, -min(Padd), -np.inf], [2, np.inf, min(Pobs)]), max_nfev=1e6)  # if the initial lambda guess is in [0, 2] interval, we try to avoid very large or very little lambda values
+                bounds=([0, -min(Padd), -np.inf], [2, np.inf, min(Pobs)]), max_nfev=1e6)  
+            # If the initial lambda guess is in [0, 2] interval, we try to avoid very large or very little lambda values
         except ValueError:
             try:
                 popt, pcov = sp.optimize.curve_fit(f=model, xdata=Padd, ydata=Pobs, sigma=0.01, p0=[lambdas[0],0,0],
-                    bounds=([-np.inf, -min(Padd), -np.inf], [np.inf, np.inf, min(Pobs)]), max_nfev=1e6) # if it is not possible, we fit with no restrictions other than mathematical
+                    bounds=([-np.inf, -min(Padd), -np.inf], [np.inf, np.inf, min(Pobs)]), max_nfev=1e6)
+                # If it is not possible, we fit with no restrictions other than mathematical
             except ValueError:
                 print('FAILED TO LINEARIZE THE HYPERCUBE\n', file=fout)
-                continue   # sometimes (very rarely) the hypercube can not be linearized using this method. in this case we give up with this hypercube and continue with other hypercubes
+                continue   
+            # Sometimes (very rarely) the hypercube can not be linearized using this method.
+            # in this case we give up with this hypercube and continue with other hypercubes
         print(popt)
         print(pcov)
         Pobs_linear = list()
         for p in Pobs:
-            Pobs_linear.append((popt[0] * sp.stats.gmean(Padd + popt[1]) ** (popt[0] - 1) * (p - popt[2]) + 1) ** (1 / popt[0]) - popt[1]) # applying reverse transform to observed phenotypes to get linearized values
+            Pobs_linear.append((popt[0] * sp.stats.gmean(Padd + popt[1]) ** (popt[0] - 1) * (p - popt[2]) + 1) ** (1 / popt[0]) - popt[1])
+            # Apply reverse transform to observed phenotypes to get linearized values
 
         for j in range(len(genotypes)):
-            fout.write(genotypes[j] + ',' + str(Pobs_linear[j]) + '\t') # output
+            fout.write(genotypes[j] + ',' + str(Pobs_linear[j]) + '\t') # Output
         fout.write('\n')
