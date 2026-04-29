3ed5230e2016a2b46e859657b0dee6b32c2ff2bf
dpetrov835 <164385826+dpetrov835@users.noreply.github.com>
2026-04-29 20:03:26 +0300

    Update bc_hcubewise.py

--- bc_hcubewise.py
+++ bc_hcubewise.py
@@ -1,27 +1,16 @@
+'''
+This is the Box-Cox linearization script for hypercube-wise computation
+'''
+
 import argparse
 import numpy as np
 import scipy as sp
 from sklearn.preprocessing import PowerTransformer
 from collections import defaultdict
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-c', '--hypercubes')
-parser.add_argument('-l', '--landscape')
-parser.add_argument('-o', '--output')
-args = parser.parse_args()
-
-# This is the Box-Cox linearization script for hypercube-wise computation
-
-land = dict()
-
-with open(args.landscape) as f:  # Parse the landscape file (genotypes in sequence format)
-    for line in f:
-        spl = line.split()
-        land[spl[0]] = spl[1]
-
 def model(Padd: np.array[float], lmbda: float, A: float, B: float):  # Define the Box-Cox based transform
-    """
-    
+    '''
+    Define the Box-Cox based transform
 
     Parameters
     ----------
@@ -39,23 +28,20 @@ def model(Padd: np.array[float], lmbda: float, A: float, B: float):  # Define th
     np.array[float]
         OBSERVED PHENOTYPES.
 
-    """
+    '''
     if lmbda !=0:
         return ((Padd + A) ** lmbda - 1) / (lmbda * sp.stats.gmean(Padd + A) ** (lmbda - 1)) + B
     else:
         return sp.stats.gmean(Padd + A) * np.log(Padd + A) + B
-
-genotypes = list()
-Pobs = list()
-
+        
 def calculate_additive_phenotypes(genotypes: list[str], phenotypes: list[float]):    # Calculate additive phenotypes
-    """
-    
+    '''
+    Calculate additive phenotypes
 
     Parameters
     ----------
     genotypes : list[str]
-        GENOTYPES.
+        GENOTYPES IN SEQUENCE FORMAT.
     phenotypes : list[float]
         OBSERVED PHENOTYPES.
 
@@ -64,7 +50,7 @@ def calculate_additive_phenotypes(genotypes: list[str], phenotypes: list[float])
     list[float]
         ADDITIVE PHENOTYPES.
 
-    """
+    '''
     n = len(genotypes)
     if n == 0:
         return []
@@ -107,9 +93,28 @@ def calculate_additive_phenotypes(genotypes: list[str], phenotypes: list[float])
 
     return additive_phenotypes
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--hypercubes')
+parser.add_argument('-l', '--landscape')
+parser.add_argument('-o', '--output')
+args = parser.parse_args()
+
+land = dict()
+
+genotypes = list()
+Pobs = list()
+
+# Parse the landscape file (genotypes in sequence format)
+with open(args.landscape) as f:
+    for line in f:
+        spl = line.split()
+        land[spl[0]] = spl[1]
 
 with open(args.hypercubes) as fin, open(args.output, 'w') as fout:
+    
     for line in fin:
+        
+        # Define each hypercube in situ
         genotypes = list()
         genotypes.append(line.split('\t')[1].strip())
         muts = line.split('\t')[0].split(':')
@@ -117,35 +122,39 @@ with open(args.hypercubes) as fin, open(args.output, 'w') as fout:
             temp = list()
             for seq in genotypes:
                 temp.append(seq[:int(mut[1:-1])]+mut[0]+seq[int(mut[1:-1])+1:])
-            genotypes.extend(temp) # Define each hypercube in situ
+            genotypes.extend(temp)
 
-        Pobs = list(map(float, [land[genotype] for genotype in genotypes])) # Find phenotypes for needed genotypes
+        # Find phenotypes for needed genotypes
+        Pobs = list(map(float, [land[genotype] for genotype in genotypes]))
         Padd = np.array(calculate_additive_phenotypes(genotypes, Pobs))
 
+        # Initial guess for lambda parameter
         pt = PowerTransformer()
         pt.fit(Padd.reshape(-1, 1))
-        lambdas = pt.lambdas_ # Initial guess for lambda parameter
+        lambdas = pt.lambdas_
         print(lambdas)
+        
         try:
+            # If the initial lambda guess is in [0, 2] interval, we try to avoid very large or very little lambda values
             popt, pcov = sp.optimize.curve_fit(f=model, xdata=Padd, ydata=Pobs, sigma=0.01, p0=[lambdas[0],0,0],   
                 bounds=([0, -min(Padd), -np.inf], [2, np.inf, min(Pobs)]), max_nfev=1e6)  
-            # If the initial lambda guess is in [0, 2] interval, we try to avoid very large or very little lambda values
         except ValueError:
             try:
+                 # If it is not possible, we fit with no restrictions other than mathematical
                 popt, pcov = sp.optimize.curve_fit(f=model, xdata=Padd, ydata=Pobs, sigma=0.01, p0=[lambdas[0],0,0],
                     bounds=([-np.inf, -min(Padd), -np.inf], [np.inf, np.inf, min(Pobs)]), max_nfev=1e6)
-                # If it is not possible, we fit with no restrictions other than mathematical
             except ValueError:
+                # Sometimes (very rarely) the hypercube can not be linearized using this method.
+                # in this case we give up with this hypercube and continue with other hypercubes
                 print('FAILED TO LINEARIZE THE HYPERCUBE\n', file=fout)
-                continue   
-            # Sometimes (very rarely) the hypercube can not be linearized using this method.
-            # in this case we give up with this hypercube and continue with other hypercubes
+                continue
         print(popt)
         print(pcov)
+
+        # Apply reverse transform to observed phenotypes to get linearized values
         Pobs_linear = list()
         for p in Pobs:
             Pobs_linear.append((popt[0] * sp.stats.gmean(Padd + popt[1]) ** (popt[0] - 1) * (p - popt[2]) + 1) ** (1 / popt[0]) - popt[1])
-            # Apply reverse transform to observed phenotypes to get linearized values
 
         for j in range(len(genotypes)):
             fout.write(genotypes[j] + ',' + str(Pobs_linear[j]) + '\t') # Output
