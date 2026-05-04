d5b47c3f33b99854091df1ba25e2171847b0c151
dpetrov835 <164385826+dpetrov835@users.noreply.github.com>
2026-05-04 11:53:09 +0300

    Refine type hints for model functions
    Updated type hints in model and model_inverse functions to use np.ndarray instead of np.array.

--- yj_single.py
+++ yj_single.py
@@ -10,13 +10,13 @@ from sklearn.preprocessing import PowerTransformer
 from scipy.stats import gmean
 from collections import defaultdict
 
-def model(P_add, lam, A, B):
+def model(P_add: np.ndarray[float], lam: float, A: float, B: floa):
     '''
     Define the Yeo-Johnson based transform
 
     Parameters
     ----------
-    Padd : np.array[float]
+    Padd : np.ndarray[float]
         ADDITIVE PHENOTYPES.
     lmbda : float
         LMBDA PARAMETER.
@@ -27,7 +27,7 @@ def model(P_add, lam, A, B):
 
     Returns
     -------
-    np.array[float]
+    np.ndarray[float]
         OBSERVED PHENOTYPES.
 
     '''
@@ -76,13 +76,13 @@ def model(P_add, lam, A, B):
     
     return P_obs
 
-def model_inverse(P_obs: np.array[float], lmbda: float, A: float, B: float, GM: float):
+def model_inverse(P_obs: np.ndarray[float], lmbda: float, A: float, B: float, GM: float):
     '''
     Define the inverse Yeo-Johnson based transform
     
     Parameters
     ----------
-    P_obs : np.array[float]
+    P_obs : np.ndarray[float]
         OBSERVED PHENOTYPES.
     lmbda : float
         LMBDA PARAMETER.
@@ -95,7 +95,7 @@ def model_inverse(P_obs: np.array[float], lmbda: float, A: float, B: float, GM: 
 
     Returns
     -------
-    np.array[float]
+    np.ndarray[float]
         LINEARIZED PHENOTYPES.
 
     '''
